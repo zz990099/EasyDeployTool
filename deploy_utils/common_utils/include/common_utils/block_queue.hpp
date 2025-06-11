dd7bd33a2e08f8bd2345e374dbbb176e7a599886
zz990099 <771647586@qq.com>
2025-06-11 20:55:29 +0800

    improve[block_queue]: Improve implementation of block_queue
    Signed-off-by: zz990099 <771647586@qq.com>

--- deploy_utils/common_utils/include/common_utils/block_queue.hpp
+++ deploy_utils/common_utils/include/common_utils/block_queue.hpp
@@ -1,161 +1,131 @@
 #pragma once
 
-#include <atomic>
 #include <condition_variable>
+#include <mutex>
 #include <optional>
 #include <queue>
 
 namespace easy_deploy {
 
 /**
- * @brief A simple implementation of block queue.
- *
- * @tparam T
+ * @brief A thread-safe blocking queue with shutdown/disable semantics.
  */
 template <typename T>
 class BlockQueue {
 public:
-  BlockQueue<T>(const size_t max_size) : max_size_(max_size)
+  explicit BlockQueue(size_t max_size) : max_size_(max_size)
   {}
+
+  // 禁拷贝、禁赋值
+  BlockQueue(const BlockQueue &)            = delete;
+  BlockQueue &operator=(const BlockQueue &) = delete;
 
   /**
    * @brief Push a obj into the queue. Will block the thread if the queue is full.
-   *
-   * @param obj
-   * @return true
-   * @return false
+   * Return false if push is disabled.
    */
   template <typename U>
   bool BlockPush(U &&obj) noexcept;
 
   /**
-   * @brief Push a obj into the queue. Will cover the oldest element if the queue is full.
-   *
-   * @param obj
-   * @return true
-   * @return false
+   * @brief Push a obj into the queue. If full, remove oldest and insert.
+   * Return false if push is disabled.
    */
   template <typename U>
   bool CoverPush(U &&obj) noexcept;
 
   /**
-   * @brief Get and pop the oldest element in the queue. Will block the thread if the queue is
-   * empty.
-   *
-   * @return std::optional<T>
+   * @brief Remove and return the front element. Will block if empty and not disabled/no more input.
+   * Return std::nullopt if take is disabled, or no more input and queue is empty.
    */
   std::optional<T> Take() noexcept;
 
   /**
-   * @brief Get and pop the oldest element in the queue. Will return `nullopt` if the queue is
-   * empty.
-   *
-   * @return std::optional<T>
+   * @brief Remove and return front element if any; else return std::nullopt.
    */
   std::optional<T> TryTake() noexcept;
 
   /**
-   * @brief Get the size of the queue.
-   *
-   * @return int
+   * @brief Return current queue size.
    */
   size_t Size() noexcept;
 
   /**
-   * @brief Return if the queue is empty.
-   *
-   * @return true
-   * @return false
+   * @brief Return if queue is empty.
    */
   bool Empty() noexcept;
 
   /**
-   * @brief Set the `push` process disabled. After called this method, all `push` calling will
-   * return `false`, which means this block queue no longer accept new elements.
-   *
-   */
-  void DisablePush() noexcept;
-
-  /**
-   * @brief Set the `push` process enabled.
-   *
-   */
-  void EnablePush() noexcept;
-
-  /**
-   * @brief Set the `take` process disabled. After called this method, all `take` calling will
-   * return `false`, which means this block queue no longer provides elements.
-   *
-   */
-  void DisableTake() noexcept;
-
-  /**
-   * @brief Set the `take` process enabled.
-   *
-   */
-  void EnableTake() noexcept;
-
-  /**
-   * @brief Set the `push` and `take` process disabled.
-   *
+   * @brief Disable both push and take. Wake up all threads.
    */
   void Disable() noexcept;
 
   /**
-   * @brief Get the max size of the block queue.
-   *
-   * @return int
-   */
-  size_t GetMaxSize() const noexcept;
-
-  /**
-   * @brief Set the `push` and `take` process disabled, and clear all elements in it.
-   *
+   * @brief Clear all elements and disable both push/take.
    */
   void DisableAndClear() noexcept;
 
   /**
-   * @brief Set the `push` process will no longer be called. The consumer threads which were
-   * blocked will be notified and quit blocking, when this method is called.
-   *
+   * @brief Disable push only. Wake up producers.
+   */
+  void DisablePush() noexcept;
+
+  /**
+   * @brief Re-enable push. Wake up waiting producers.
+   */
+  void EnablePush() noexcept;
+
+  /**
+   * @brief Disable take only. Wake up consumers.
+   */
+  void DisableTake() noexcept;
+
+  /**
+   * @brief Re-enable take. Wake up waiting consumers.
+   */
+  void EnableTake() noexcept;
+
+  /**
+   * @brief Set "NoMoreInput", i.e. producers不会再推送, 通知所有消费者。
    */
   void SetNoMoreInput() noexcept;
 
-  ~BlockQueue() noexcept;
+  /**
+   * @brief Get max size.
+   */
+  size_t GetMaxSize() const noexcept
+  {
+    return max_size_;
+  }
+
+  ~BlockQueue() noexcept
+  {
+    Disable();
+  }
 
 private:
-  const size_t            max_size_;
+  size_t                  max_size_;
   std::queue<T>           q_;
-  std::atomic<bool>       push_enabled_{true};
-  std::atomic<bool>       take_enabled_{true};
-  std::condition_variable producer_cv_;
-  std::condition_variable consumer_cv_;
-  std::mutex              lck_;
-
-  std::atomic<bool> no_more_input_{false};
+  bool                    push_enabled_{true};
+  bool                    take_enabled_{true};
+  bool                    no_more_input_{false};
+  std::mutex              mtx_;
+  std::condition_variable cv_producer_;
+  std::condition_variable cv_consumer_;
 };
 
-template <typename T>
-BlockQueue<T>::~BlockQueue() noexcept
-{
-  Disable();
-}
+// ========== Implementation ==========
 
 template <typename T>
 template <typename U>
 bool BlockQueue<T>::BlockPush(U &&obj) noexcept
 {
-  std::unique_lock<std::mutex> u_lck(lck_);
-  while (q_.size() >= max_size_ && push_enabled_.load())
-  {
-    producer_cv_.wait(u_lck);
-  }
-  if (!push_enabled_.load())
-  {
+  std::unique_lock<std::mutex> lk(mtx_);
+  cv_producer_.wait(lk, [this] { return q_.size() < max_size_ || !push_enabled_; });
+  if (!push_enabled_)
     return false;
-  }
-  q_.push(std::forward<U>(obj)); // 完美转发
-  consumer_cv_.notify_one();
+  q_.push(std::forward<U>(obj));
+  cv_consumer_.notify_one();
   return true;
 }
 
@@ -163,130 +133,118 @@ template <typename T>
 template <typename U>
 bool BlockQueue<T>::CoverPush(U &&obj) noexcept
 {
-  std::unique_lock<std::mutex> u_lck(lck_);
-  if (!push_enabled_.load())
-  {
+  std::unique_lock<std::mutex> lk(mtx_);
+  if (!push_enabled_)
     return false;
-  }
   if (q_.size() == max_size_)
-  {
     q_.pop();
-  }
   q_.push(std::forward<U>(obj));
-  consumer_cv_.notify_one();
+  cv_consumer_.notify_one();
   return true;
 }
 
 template <typename T>
 std::optional<T> BlockQueue<T>::Take() noexcept
 {
-  std::unique_lock<std::mutex> u_lck(lck_);
-  // block until: 1. take disabled; 2. no more input set; 3. new elements
-  while (q_.size() == 0 && take_enabled_ && no_more_input_ == false)
-  {
-    consumer_cv_.wait(u_lck);
-  }
-  if (!take_enabled_ || (no_more_input_ && q_.size() == 0))
-  {
+  std::unique_lock<std::mutex> lk(mtx_);
+  cv_consumer_.wait(lk, [this] { return !q_.empty() || !take_enabled_ || no_more_input_; });
+  if (!take_enabled_ || (no_more_input_ && q_.empty()))
     return std::nullopt;
-  }
-  T ret = std::move(q_.front());
+  T obj = std::move(q_.front());
   q_.pop();
-  producer_cv_.notify_one();
-
-  if (no_more_input_)
-  {
-    consumer_cv_.notify_all();
-  }
-  return ret;
+  cv_producer_.notify_one();
+  return obj;
 }
 
 template <typename T>
 std::optional<T> BlockQueue<T>::TryTake() noexcept
 {
-  std::unique_lock<std::mutex> u_lck(lck_);
-  if (q_.size() == 0)
-  {
+  std::unique_lock<std::mutex> lk(mtx_);
+  if (q_.empty())
     return std::nullopt;
-  } else
-  {
-    T ret = q_.front();
-    q_.pop();
-    producer_cv_.notify_all();
-    if (no_more_input_)
-    {
-      consumer_cv_.notify_all();
-    }
-    return ret;
-  }
+  T obj = std::move(q_.front());
+  q_.pop();
+  cv_producer_.notify_one();
+  return obj;
 }
 
 template <typename T>
 size_t BlockQueue<T>::Size() noexcept
 {
-  std::unique_lock<std::mutex> u_lck(lck_);
+  std::lock_guard<std::mutex> lk(mtx_);
   return q_.size();
 }
 
 template <typename T>
 bool BlockQueue<T>::Empty() noexcept
 {
-  std::unique_lock<std::mutex> u_lck(lck_);
-  return q_.size() == 0;
-}
-
-template <typename T>
-size_t BlockQueue<T>::GetMaxSize() const noexcept
-{
-  return max_size_;
+  std::lock_guard<std::mutex> lk(mtx_);
+  return q_.empty();
 }
 
 template <typename T>
 void BlockQueue<T>::Disable() noexcept
 {
-  DisablePush();
-  DisableTake();
+  std::lock_guard<std::mutex> lk(mtx_);
+  push_enabled_  = false;
+  take_enabled_  = false;
+  no_more_input_ = true;
+  cv_producer_.notify_all();
+  cv_consumer_.notify_all();
 }
 
 template <typename T>
 void BlockQueue<T>::DisableAndClear() noexcept
 {
-  Disable();
-  std::unique_lock<std::mutex> u_lck(lck_);
+  std::lock_guard<std::mutex> lk(mtx_);
+  push_enabled_  = false;
+  take_enabled_  = false;
+  no_more_input_ = true;
   while (!q_.empty()) q_.pop();
+  cv_producer_.notify_all();
+  cv_consumer_.notify_all();
 }
 
 template <typename T>
 void BlockQueue<T>::DisablePush() noexcept
 {
-  push_enabled_.store(false);
-  producer_cv_.notify_all();
+  std::lock_guard<std::mutex> lk(mtx_);
+  push_enabled_ = false;
+  cv_producer_.notify_all();
+  cv_consumer_.notify_all();
 }
 
 template <typename T>
 void BlockQueue<T>::EnablePush() noexcept
 {
-  push_enabled_.store(true);
+  std::lock_guard<std::mutex> lk(mtx_);
+  push_enabled_ = true;
+  cv_producer_.notify_all();
 }
 
 template <typename T>
 void BlockQueue<T>::DisableTake() noexcept
 {
-  take_enabled_.store(false);
-  consumer_cv_.notify_all();
+  std::lock_guard<std::mutex> lk(mtx_);
+  take_enabled_ = false;
+  cv_producer_.notify_all();
+  cv_consumer_.notify_all();
 }
 
 template <typename T>
 void BlockQueue<T>::EnableTake() noexcept
 {
-  take_enabled_.store(true);
+  std::lock_guard<std::mutex> lk(mtx_);
+  take_enabled_ = true;
+  cv_consumer_.notify_all();
 }
 
 template <typename T>
 void BlockQueue<T>::SetNoMoreInput() noexcept
 {
-  no_more_input_.store(true);
-  consumer_cv_.notify_all();
+  std::lock_guard<std::mutex> lk(mtx_);
+  no_more_input_ = true;
+  cv_consumer_.notify_all();
 }
 
 } // namespace easy_deploy
